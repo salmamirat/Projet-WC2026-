06c76c06d59d8d2ca40d0346161f6e962055f496
salmamirat <salmamirat@gmail.com>
2026-07-13 11:02:42 +0200

    update

--- middlewares/validate.middleware.js
+++ middlewares/validate.middleware.js
@@ -1,13 +1,15 @@
-const validate = (req, res, next)=>{
+const validate = (req, res, next) => {
     const data = req.body;
 
-    for (const key in data){
-        if(data[key] === null || data[key] === ""){
+    for (const key in data) {
+        if (data[key] === null || data[key] === "") {
             return res.status(400).json({
-                message:`${key} est obligatoire`
+                message: `${key} est obligatoire`
             });
         }
     }
+
     next();
 };
+
 module.exports = validate;
--- routes/arbitre.routes.js
+++ routes/arbitre.routes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const arbitreController = require("../controllers/arbitre.controller"); 
-const { validate } = require("../middlewares/validate.middleware");
+const  validate  = require("../middlewares/validate.middleware");
 const { authenticate } = require("../middlewares/auth.middleware");
 const { authorize } = require("../middlewares/role");
 
 
-router.post("/",authenticate,authorize("admin","commissaire"),validate, arbitreController.createArbitre);
+
+
+router.post("/",authenticate,authorize("admin"),validate, arbitreController.createArbitre);
 router.get("/",  authenticate, arbitreController.getAllArbitres);
 router.get("/:id", authenticate, arbitreController.getArbitreById);
 router.put("/:id", authenticate, authorize("admin") , validate, arbitreController.updateArbitre);
